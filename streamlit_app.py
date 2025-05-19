4fa1ed58941241d45f8c4f8f884791ab4fd49f95
Ryan Richardson <116780532+rmrichardson88@users.noreply.github.com>
2025-05-19 16:55:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,14 @@ from urllib.parse import urlparse
 SPOTIFY_CLIENT_ID = st.secrets["SPOTIFY_CLIENT_ID"]
 SPOTIFY_CLIENT_SECRET = st.secrets["SPOTIFY_CLIENT_SECRET"]
 REDIRECT_URI = st.secrets["SPOTIPY_REDIRECT_URI"]
-scope = "playlist-read-private playlist-read-collaborative"
+SCOPE = "playlist-read-private playlist-read-collaborative"
 
 def get_spotify_client():
     sp_oauth = SpotifyOAuth(
         client_id=SPOTIFY_CLIENT_ID,
         client_secret=SPOTIFY_CLIENT_SECRET,
         redirect_uri=REDIRECT_URI,
-        scope=SCOPE,
+        SCOPE=SCOPE,
     )
 
     token_info = st.session_state.get("token_info", None)
