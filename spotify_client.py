f4373ae0906aadcdce3619230944b89526b7c971
Ryan Richardson <116780532+rmrichardson88@users.noreply.github.com>
2025-05-19 16:25:48 -0600

    Update spotify_client.py

--- spotify_client.py
+++ spotify_client.py
@@ -1,43 +1,11 @@
-import spotipy
-from spotipy.oauth2 import SpotifyPKCE
 import pandas as pd
-import os
-from urllib.parse import urlparse, parse_qs
-import streamlit as st
+from urllib.parse import urlparse
 
 def extract_playlist_id(url):
     path = urlparse(url).path
     return path.split("/")[-1]
 
-def get_playlist_audio_features(playlist_url):
-    sp_oauth = SpotifyPKCE(
-        client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-        redirect_uri="https://spotifypreferenceexplorer.streamlit.app/",
-        scope="playlist-read-private playlist-read-collaborative"
-    )
-
-
-    token_info = sp_oauth.get_cached_token()
-    if not token_info:
-        auth_url = sp_oauth.get_authorize_url()
-        st.warning("Please authenticate with Spotify below:")
-        st.markdown(f"[Click here to authorize]({auth_url})")
-        auth_code = st.text_input("After authorizing, paste the full redirected URL here:")
-
-        if auth_code:
-            parsed = urlparse(auth_code)
-            code = parse_qs(parsed.query).get("code", [None])[0]
-
-            if not code:
-                raise ValueError("Could not extract authorization code.")
-
-            token_info = sp_oauth.get_access_token(code)
-
-    if not token_info:
-        raise ValueError("Authentication failed. Please check your client ID and URL.")
-
-    sp = spotipy.Spotify(auth=token_info["access_token"])
-
+def get_playlist_audio_features(sp, playlist_url):
     playlist_id = extract_playlist_id(playlist_url)
     results = sp.playlist_tracks(playlist_id, limit=100, market='US')
     tracks = results["items"]
